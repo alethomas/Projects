9791b3e0fe3c63a1b4ccee7603aa77b30657b41f
Alexander Thomas <alex.thomas89@gmail.com>
2021-01-20 00:31:12 +0100

    Corrections

--- Docker_Training/Docker_Snakemake_BLAST/data/add_annotation.py
+++ Docker_Training/Docker_Snakemake_BLAST/data/add_annotation.py
@@ -1,15 +1,24 @@
 # add KEGG annotations to the blast
 
-folder = "/Users/alex/Projects/Docker_Training/Docker_Snakemake_BLAST/data"
+import pprint
+
+folder = "/Users/alex/Projects/Docker_Training/Docker_Snakemake_BLAST/data/"
 
 infile = open(folder + "uniprot_sprot.dat")
 
 AC2KEGG = {}
 
 for line in infile.read().splitlines():
-    if line.startswith("AC")
+    if line.startswith("AC"):
         KEGG = ""
-        AC = line.split("\t")[1]
-    elif line.startswith("DR") and line.split("\t")[1].startswith("KEGG")
+        AC = line.split("   ")[1]
+    elif line.startswith("DR") and line.split("   ")[1].startswith("KEGG"):
+        Accession = line.split("; ")[1]
         if AC in AC2KEGG:
-            AC2KEGG[AC].append()
+            AC2KEGG[AC].append(Accession)
+        else:
+            AC2KEGG[AC] = [Accession]
+
+# for key in AC2KEGG:
+#     print(key + "\t" + AC2KEGG[key])
+pprint.pprint(AC2KEGG)
